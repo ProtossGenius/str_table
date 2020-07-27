28691785d43671023914f3553c706af204f8a55f
suremoon <protossgenius@gmail.com>
2020-07-27 22:29:34 +0800

    some change about example

--- example/emp.go
+++ example/emp.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/ProtossGenius/str_table"
+	"github.com/mattn/go-runewidth"
 )
 
 type Struct struct {
@@ -12,6 +13,8 @@ type Struct struct {
 }
 
 func main() {
+	fmt.Println(runewidth.StringWidth("A哈哈哈"))
+	fmt.Println("012345678901234567890123456789")
 	fmt.Println(str_table.TableLineStr(str_table.TableLineType_FIRST_LINE, []int{6, 6, 10, 10}))
 	t := str_table.StructToTable("hello", []interface{}{&Struct{A: "hello", B: "World"}})
 	t.AddRow([]string{"cccccc", "dddddd"})
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ProtossGenius/str_table
+
+go 1.14
+
+require github.com/mattn/go-runewidth v0.0.9
